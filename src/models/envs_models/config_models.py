ad3ddaa6d31ac472f7ddb98ae37d401a8c552f70
AST-LW <walkthroughfrom2020@gmail.com>
2024-05-12 15:01:37 +0530

    priortize properties feature

--- src/models/envs_models/config_models.py
+++ src/models/envs_models/config_models.py
@@ -1,12 +1,16 @@
+import os
 from pydantic.dataclasses import dataclass
 
 
 @dataclass
 class ManifestModel:
     BASE_URL: str = ""
+    CAPTURE_LOGS: bool = False
+    ENV: str = ""
 
 
 @dataclass
 class BaseConfigModel:
+    main_manifest_file_path: str = os.path.join(os.getcwd(), "manifest.toml")
     manifest_file_path: str = ""
     endpoints_path: str = ""
--- src/utils/config_parser.py
+++ src/utils/config_parser.py
@@ -8,7 +8,7 @@ SUPPORTED_ENVS = ["qa"]
 
 
 @dataclass
-class QaConfig(BaseConfigModel):
+class QaConfigModel(BaseConfigModel):
     manifest_file_path: str = os.path.join(
         os.getcwd(), "envs", "qa", "manifest.toml")
     endpoints_path: str = os.path.join(
@@ -18,24 +18,54 @@ class QaConfig(BaseConfigModel):
 
 
 def _select_environment_paths() -> BaseConfigModel:
-    # by default the QA config will be auto selected
-    env = os.getenv("ENV") or "qa"
+    # Select the environment given in the main manifest file
+    main_manifest_file = BaseConfigModel().main_manifest_file_path
+    main_manifest_data = load_toml_config(main_manifest_file)
+
+    # ENV from CLI is prioritized over main manifest file
+    env = os.getenv("ENV") or ManifestModel(**main_manifest_data).ENV
+
+    # Raise the exception if the environment is not specified
+    if env == "" or None:
+        raise Exception(
+            "The environment on which test suite execution is missing, specify in either CLI as environment variables or provide in main manifest file")
+
+    # Raise the exception if the environment is not supported environments
     if env.lower() not in SUPPORTED_ENVS:
         raise Exception(f"{env} is not supported environment")
 
+    # Choose the environment data model
     if env == "qa":
-        return QaConfig()
+        return QaConfigModel()
+    else:
+        raise Exception(
+            f"The specified environment - {env}, is not configured")
 
 
 def fetch_config() -> ManifestModel:
-    manifest_file = _select_environment_paths()
-    manifest_data = load_toml_config(manifest_file.manifest_file_path)
-    return ManifestModel(**manifest_data)
+    # config priority -> Main manifest file < Environment specific manifest file < CLI property passing as environment variables of the system / command line arguments
+    selected_environment = _select_environment_paths()
+
+    main_manifest_data = load_toml_config(
+        selected_environment.main_manifest_file_path)
+    environment_manifest_data = load_toml_config(
+        selected_environment.manifest_file_path)
+
+    # The order is important as the later having the same property will override the earlier one
+    # As the environment level manifest file has more priority compared to main manifest file present at root level
+    merged_manifest_data = {**main_manifest_data, **environment_manifest_data}
+
+    # After merging the environment data, we can check if any environment variable is passed via CLI, then we need to replace with CLI one
+    for key, value in merged_manifest_data.items():
+        if os.getenv(key) is not None:
+            merged_manifest_data[key] = os.getenv(key)
+
+    return ManifestModel(**merged_manifest_data)
 
 
 def fetch_endpoints():
-    endpoints_file = _select_environment_paths()
-    endpoints_data = load_toml_config(endpoints_file.endpoints_path)
+    selected_environment = _select_environment_paths()
+    endpoints_data = load_toml_config(selected_environment.endpoints_path)
     return EndpointsModel(**endpoints_data)
 
 
