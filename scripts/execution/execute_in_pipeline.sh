3ad08edbc0c9129aef8f316aaed1775dfbf6f9d2
AST-LW <walkthroughfrom2020@gmail.com>
2024-06-01 18:06:26 +0530

    testing

--- scripts/execution/execute_in_pipeline.sh
+++ scripts/execution/execute_in_pipeline.sh
@@ -7,4 +7,5 @@ chmod +x scripts/util-scripts/generate-allure-report.sh
 scripts/util-scripts/generate-allure-report.sh
 
 # extract the test execution summary
+touch test-summary.json
 python src/utils/test_summary_extractor.py
